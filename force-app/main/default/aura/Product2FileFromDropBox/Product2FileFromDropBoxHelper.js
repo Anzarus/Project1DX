133e57f1425a5a7423b354048b99bec1cbe841f0
Anton Antoniuk <antonantonuk1999@gmail.com>
2019-09-24 19:17:16 +0300

    todo task

--- force-app/main/default/aura/Product2FileFromDropBox/Product2FileFromDropBoxHelper.js
+++ force-app/main/default/aura/Product2FileFromDropBox/Product2FileFromDropBoxHelper.js
@@ -18,7 +18,7 @@
             }
         }).catch(function (errors) {
             let errorMessage = 'Unknown error';
-            if(errors && Array.isArray(errors) && errors.length > 0){
+            if (errors && Array.isArray(errors) && errors.length > 0) {
                 errorMessage = errors[0].message;
             }
             toastCmp.showToast($A.get("$Label.c.err"), errorMessage, 'error');
@@ -35,14 +35,10 @@
             "getFileForThisRecord",
             {recordId: recordId}
         ).then(function (result) {
-            if (result.toString().startsWith("Error")) {
-                toastCmp.showToast("Error", result, "error");
-            } else {
-                window.open(result.toString(), "_self");
-            }
+            window.open(result.toString(), "_self");
         }).catch(function (errors) {
             let errorMessage = 'Unknown error';
-            if(errors && Array.isArray(errors) && errors.length > 0){
+            if (errors && Array.isArray(errors) && errors.length > 0) {
                 errorMessage = errors[0].message;
             }
             toastCmp.showToast($A.get("$Label.c.err"), errorMessage, 'error');
